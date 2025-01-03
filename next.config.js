1a7c1f019b7f3461f722c83471354952b3be977b
Eugene Kontelev <491908+kontel@users.noreply.github.com>
2025-01-03 14:12:51 -0500

    Enhance Next.js configuration with experimental server actions and update image remote patterns
    - Added experimental server actions with a body size limit of 10mb to next.config.js.
    - Updated image remote patterns to allow any hostname, improving flexibility for image sources.
    - Cleaned up code formatting for consistency.

--- next.config.js
+++ next.config.js
@@ -1,19 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
+  },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "**",
       },
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-      }
     ],
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
